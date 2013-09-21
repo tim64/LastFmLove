95e573b0313d09e71eb1cd8fa0019d339157bb7d
tim64 <zappazoid@gmail.com>
2013-09-21 13:23:17 +0700

    Correct logic. Add flac, ogg format.

--- src/lastfmlove/Scaner.java
+++ src/lastfmlove/Scaner.java
@@ -11,11 +11,22 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.AudioHeader;
 import org.jaudiotagger.audio.mp3.MP3AudioHeader;
 import org.jaudiotagger.audio.mp3.MP3File;
+import org.jaudiotagger.audio.flac.FlacAudioHeader;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
-
+import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
+import org.jaudiotagger.tag.id3.ID3v24Frames;
+import org.farng.mp3.TagException;
+import org.farng.mp3.id3.*;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.tag.flac.FlacTag;
+import org.jaudiotagger.tag.mp4.Mp4FieldKey;
+import org.jaudiotagger.tag.mp4.Mp4Tag;
+import org.jaudiotagger.tag.vorbiscomment.VorbisCommentFieldKey;
+import org.jaudiotagger.tag.vorbiscomment.VorbisCommentTag;
 /**
  *
  * @author Тим
@@ -25,49 +36,61 @@ public class Scaner {
     public void scan_root(String root_dir, ArrayList<Song> song_list) throws IOException, Exception {
         FileFinder f = new FileFinder();
         List dirs;
-        String input;
+        String path, input;
         Pattern pattern;
         Matcher matcher;
         dirs = f.findDirectories(root_dir);
         for (int s = 0; s < song_list.size(); s++)
         {
-            pattern = Pattern.compile(song_list.get(s).artist);
-                 if (!pattern.pattern().isEmpty())
-                 {
+            String artist = song_list.get(s).artist.toLowerCase().replace(" ", "");
+            //pattern = Pattern.compile(song_list.get(s).artist);
+                 //if (!pattern.pattern().isEmpty())
+                 //{
                     for (int d = 0; d < dirs.size(); d++)
                     {
-                        input = dirs.get(d).toString();
-                        matcher = pattern.matcher(input); 
-                        if (matcher.find()) {
-                            folder_path_creator(input, song_list, s);
+                        path = dirs.get(d).toString();
+                        input = dirs.get(d).toString().toLowerCase().replace(" ", "");
+                        //matcher = pattern.matcher(input); 
+                        //if (matcher.) {
+                        if (input.indexOf(artist) > -1) {
+                            folder_path_creator(path, song_list, s);
                         }
                     }
-                 }
+                 //}
         }   
     }
     
     public void folder_path_creator(String root_dir, ArrayList<Song> song_list, int ind) throws IOException {
-        int count = 0;
         List paths;
-        MP3File mp3_file;
+        String filename;
         FileFinder f = new FileFinder();
             try {
                 paths = f.findFiles(root_dir, song_list.get(ind).title);
                 for (int i = 0; i < paths.size(); i++)
                 {
-                    File source_file = new File(paths.get(i).toString());                 
-                    mp3_file = (MP3File)AudioFileIO.read(source_file);
-                    if (mp3_file.getID3v1Tag().getFirstTitle().equals(song_list.get(ind).title))
-                    {
-                        count++;
-                        Song tmp = song_list.get(ind);
-                        tmp.set_path(paths.get(i).toString());
-                        song_list.set(ind, tmp);
-                    }
+                    filename = paths.get(i).toString();
+                    File source_file = new File(filename); 
+                    AudioFile af = AudioFileIO.read(source_file);
+                    Tag tag = af.getTag();
+                    AudioHeader head = af.getAudioHeader();
+                    if (tag.getFirst(FieldKey.TITLE).equals(song_list.get(ind).title))
+                            {
+                                
+                                Song tmp = song_list.get(ind);
+                                tmp.set_len(head.getTrackLength());
+                                tmp.set_path(paths.get(i).toString());
+                                song_list.set(ind, tmp);
+                            }
                 }
             }
             catch (Exception e) {
-                //System.out.println("no read "+song_list.get(ind).title);
         }
-    } 
+    }
+    
+    public static String getFileExtention(String filename){
+        int dotPos = filename.lastIndexOf(".") + 1;
+        return filename.substring(dotPos);
+    }
+    
+
 }
