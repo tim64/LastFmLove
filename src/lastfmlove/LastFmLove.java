5b971ca8e90a712d34a8829f1233130c97d951cb
tim64 <zappazoid@gmail.com>
2013-09-21 14:16:03 +0700

    Add Playlistcreator

--- src/lastfmlove/LastFmLove.java
+++ src/lastfmlove/LastFmLove.java
@@ -15,23 +15,14 @@ class LastFmLove {
     
     public static void main (String[] args) throws IOException, Exception
     {
-        int sum = 0;
         ArrayList<Song> song_list;
         Downloader d = new Downloader();
         Scaner s = new Scaner();
+        PlaylistCreator p = new PlaylistCreator();
         String url = d.get_request("tim32", "3000");
         song_list = d.read_xml(url); 
         s.scan_root("D:\\Music", song_list);
-        System.out.println("Paths");
-        for (int i  = 0; i < song_list.size(); i++)
-        {
-            if (!song_list.get(i).path.isEmpty())
-            {
-            sum++;   
-            System.out.println(song_list.get(i).path);
-            }
-        }
-        System.out.println("In all: "+song_list.size()+" Found: "+sum);
+        p.create(song_list);
         
     }
 }
