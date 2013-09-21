421512da9d4ac8a1db27e07ab2486c03d5ed288e
tim64 <zappazoid@gmail.com>
2013-09-21 14:15:46 +0700

    fix

--- src/lastfmlove/PlaylistCreator.java
+++ src/lastfmlove/PlaylistCreator.java
@@ -17,16 +17,20 @@ public class PlaylistCreator {
     
     public void create(ArrayList<Song> song_list) throws FileNotFoundException {
          PrintWriter pls;
+         int n = 0;
          pls = new PrintWriter(new FileOutputStream("lastfmlove.pls"));
          pls.println("[playlist]");
          for (int i = 0; i < song_list.size(); i++)
          {
-             int n = i + 1;
-             pls.println("File"+n+"="+song_list.get(i).path);
-             pls.println("Length"+n+"="+song_list.get(i).length);
-             pls.println("NumberOfEntries="+n);
-             pls.println("Version=2");  
+             if (!song_list.get(i).path.isEmpty() && song_list.get(i).length != 0)
+             {
+                n++;
+                pls.println("File"+n+"="+song_list.get(i).path);
+                pls.println("Length"+n+"="+song_list.get(i).length);
+             }
          }
+         pls.println("NumberOfEntries="+n);
+         pls.println("Version=2");  
          pls.close();
         
     }
